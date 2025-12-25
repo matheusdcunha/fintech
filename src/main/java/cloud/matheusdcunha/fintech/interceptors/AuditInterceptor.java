ce0490e5214a1a888915a6f13191d799ff15569c
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-12-25 00:56:38 -0300

    feat: adiciona interceptor para fazer a auditoria de cada requisicao, fazendo log

---
+++ src/main/java/cloud/matheusdcunha/fintech/interceptors/AuditInterceptor.java
@@ -0,0 +1,44 @@
+package cloud.matheusdcunha.fintech.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class AuditInterceptor implements HandlerInterceptor {
+
+    private final Logger logger = LoggerFactory.getLogger(AuditInterceptor.class);
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response,
+                           Object handler,
+                           @Nullable ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler,
+                                @Nullable Exception ex) throws Exception {
+        logger.info("Audit \n Method: {} \n URL: {} \n StatusCode: {} \n IpAddress: {} ",
+                request.getMethod(),
+                request.getRequestURI(),
+                response.getStatus(),
+                request.getAttribute("x-user-ip")
+                );
+    }
+}
---
+++ src/main/java/cloud/matheusdcunha/fintech/interceptors/InterceptorConfig.java
@@ -0,0 +1,21 @@
+package cloud.matheusdcunha.fintech.interceptors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final AuditInterceptor auditInterceptor;
+
+    public InterceptorConfig(AuditInterceptor auditInterceptor) {
+        this.auditInterceptor = auditInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(this.auditInterceptor)
+                .order(0);
+    }
+}
