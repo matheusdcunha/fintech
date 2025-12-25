e829d39b88acbedfcd1614237652695b3899fe3f
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-12-25 11:16:56 -0300

    chore: muda o nome do arquivo de entrada da aplicacao

--- src/main/java/cloud/matheusdcunha/fintech/Application.java
+++ src/main/java/cloud/matheusdcunha/fintech/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FintechApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(FintechApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
