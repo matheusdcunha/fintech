655a49f424cf46022f323056eba3e4fc7bfa76bf
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-12-24 23:07:36 -0300

    feat: adiciona entidades e suas documentação

---
+++ src/main/java/cloud/matheusdcunha/fintech/entities/Wallet.java
@@ -0,0 +1,71 @@
+package cloud.matheusdcunha.fintech.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_wallets")
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "wallet_id")
+    private UUID walletId;
+
+    @Column(name = "cpf", unique = true)
+    private String cpf;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "balance")
+    private BigDecimal balance;
+
+    public Wallet() {
+    }
+
+    public UUID getWalletId() {
+        return walletId;
+    }
+
+    public void setWalletId(UUID walletId) {
+        this.walletId = walletId;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
